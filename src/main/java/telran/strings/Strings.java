924fe6ee379241a0aa24ae2502194a527234b347
Kiril <fanky.yahoo@gmail.com>
2024-08-01 01:19:50 +0300

    added method isArithmetic expression and test for it

--- src/main/java/telran/strings/Strings.java
+++ src/main/java/telran/strings/Strings.java
@@ -1,6 +1,17 @@
 package telran.strings;
 
+import java.util.Arrays;
+
 public class Strings {
+    static final String keyWords[] = { "abstract", "assert", "boolean",
+                "break", "byte", "case", "catch", "char", "class", "const",
+                "continue", "default", "do", "double", "else", "enum", "extends", "false",
+                "final", "finally", "float", "for", "goto", "if", "implements",
+                "import", "instanceof", "int", "interface", "long", "native",
+                "new", "null", "package", "private", "protected", "public",
+                "return", "short", "static", "strictfp", "super", "switch",
+                "synchronized", "this", "throw", "throws", "transient", "true",
+                "try", "void", "volatile", "while" };
     public static String firstName () {
         //regex for strings starting with capital letter and rest as lower case
         //minimal length is 5 
@@ -8,6 +19,73 @@ public class Strings {
     }
     public static String javaVariable () {
         //regular expression for testing syntax of Java variable names
-        return "[A-Za-z][0-9A-Za-z_$]*$";
+        return "((?!_$)[a-zA-Z$_][\\w$]*)";
+    }
+    public static String number0_300() {
+        return "[1-9]\\d?|[1-2]\\d\\d|300|0";
+    }
+    public static String ipV4Octet(){
+        return "([0-1]?\\d{1,2}|2([0-4]\\d|5[0-5]))";
+    }
+    public static String ipV4Address(){
+        String octetExpr = ipV4Octet();
+        return String.format("%s(\\.%s){3}", octetExpr, octetExpr);
+    }
+    public static String stringWithJavaNames(String names) {
+       String [] tokens = names.split("\\s+");
+       int i = getJavaNameIndex(tokens, -1);
+       String res = "";
+       if (i >= 0) {
+         res = tokens[i];
+       }
+       while((i = getJavaNameIndex(tokens, i)) > 0) {
+            res += " " + tokens[i];
+       }
+        return res;
+    }
+
+    private static int getJavaNameIndex(String[] tokens, int i) {
+        i++;
+        while(i < tokens.length && !isJavaName(tokens[i])) {
+            i++;
+        }
+        return i < tokens.length ? i : -1;
+    }
+
+    private static boolean isJavaName(String string) {
+
+        return string.matches(javaVariable()) && Arrays.binarySearch(keyWords, string) < 0;
+    }
+    public static boolean isArithmeticExpression(String expr) {
+    //TODO
+    //1. brackets
+    //right position of open / close bracket is matter of regex
+    //matching between open and close bracket is matter of the method you are supposed to write
+    //based on a counter. If counter is negative - no matching;
+    // if at ending up going through a string the counter doesn't equal 0 - no matching
+    //matching may be only in one case: at the ending up of going the counter will be 0
+    // Operator - regular expression for one out of 4 arithemetic operators [*/+-]
+    //Operand may be either Java variable name or number (better any)
+        return expr.matches(arithmeticExpression()) && bracketsChecker(expr);
+    }
+
+    private static String arithmeticExpression() {
+        String operand = number();
+        String variable = javaVariable();
+        return String.format("-?[\\(\\s]*-?(%s|%s)([\\)\\s]*[-+*/][\\(\\s]*-?(%s|%s)[\\)\\s]*)+", operand, variable, operand,variable);
+    }
+
+    private static String number() {
+        return "(\\d+|\\d+\\.\\d+)";
+    }
+
+    public static boolean bracketsChecker(String expr) {
+        int bracket = 0;
+        for (int i = 0; i < expr.length(); i++) {
+            if (expr.charAt(i) == '(') bracket ++;
+            if (expr.charAt(i) == ')') bracket --;
+            if (bracket < 0) break;
+        }
+        return bracket == 0 ? true : false;
     }
 }
--- src/test/java/telran/strings/RegularExpressionsTest.java
+++ src/test/java/telran/strings/RegularExpressionsTest.java
@@ -7,17 +7,57 @@ import org.junit.jupiter.api.Test;
 
 public class RegularExpressionsTest {
     @Test
-    void javaVariableTest() {
+    void javaVariableTrueTest() {
         assertTrue("variableName".matches(javaVariable()));
         assertTrue("variable_Name1".matches(javaVariable()));
         assertTrue("VariableName".matches(javaVariable()));
         assertTrue("NUM_GEARS".matches(javaVariable()));
         assertTrue("variable$Name_1234".matches(javaVariable()));
+        assertTrue("_VarName".matches(javaVariable()));
+        assertTrue("$_varName".matches(javaVariable()));
+    }
+    @Test
+    void javaVariableFalseTest () {
         assertFalse("variable Name".matches(javaVariable()));
         assertFalse("1variableName".matches(javaVariable()));
-        assertFalse("_VarName".matches(javaVariable()));
-        assertFalse("$_varName".matches(javaVariable()));
-
+        assertFalse("1".matches(javaVariable()));
+        assertFalse("1name".matches(javaVariable()));
+        assertFalse("1_a".matches(javaVariable()));
+        assertFalse("_".matches(javaVariable()));
+        assertFalse("#1".matches(javaVariable()));
+        assertFalse("_*".matches(javaVariable())); 
+        assertFalse("ab_cd_$12?".matches(javaVariable()));
+        assertFalse("ab_cd_$12 ".matches(javaVariable()));
+    }
+    @Test
+    void isArithmeticExpressionTrueTest() {
+        assertTrue(isArithmeticExpression("10 - $var15"));
+        assertTrue(isArithmeticExpression("-8 / -2.5 + 4"));
+        assertTrue(isArithmeticExpression("(abs / 2) + (4)"));
+        assertTrue(isArithmeticExpression("((3 + (4.9 - (-2 * -var))))"));
+    }
+    @Test
+    void isArithmeticExpressionFalseTest() {
+        assertFalse(isArithmeticExpression("(3( + )5"));
+        assertFalse(isArithmeticExpression("(3 +/ 5)"));
+        assertFalse(isArithmeticExpression("(  ) - 2 * 3"));
+        assertFalse(isArithmeticExpression("(10 - 2 * 3var)"));
+        assertFalse(isArithmeticExpression("((10 - _$) * 3.923))"));
+        assertFalse(isArithmeticExpression("(10 - gtr) * )3("));
+    }
+    @Test
+    void bracketsCheckerTrueTest () {
+        assertTrue(bracketsChecker("(10 - 5)"));
+        assertTrue(bracketsChecker("((10) - (5))"));
+        assertTrue(bracketsChecker("((((10)) - ((5))))"));
+    }
+    @Test
+    void bracketsCheckerFalseTest () {
+        assertFalse(bracketsChecker("((10 - 5)"));
+        assertFalse(bracketsChecker("((10) - (5)))"));
+        assertFalse(bracketsChecker("((((10)))) - )5))))"));
+        assertFalse(bracketsChecker(")10)) - ((5))))"));
+        assertFalse(bracketsChecker("((((10)) - ((5"));
 
     }
 }
